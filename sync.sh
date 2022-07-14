721866ea657635e26d5f21d8efe8dd46651a6999
samer bahri <samer.bahi@thebahrimedia.com>
2022-07-15 01:56:13 +0200

    npm yarn zsh bash git conda

--- sync.sh
+++ sync.sh
@@ -4,6 +4,22 @@ ln -s ~/.dotfiles/.ssh/config ~/.ssh
 
 ln -s ~/.dotfiles/.pam_environment ~/
 
+ln -s ~/.dotfiles/.gitconfig ~/
+
+ln -s ~/.dotfiles/.npmrc ~/
+
+ln -s ~/.dotfiles/.yarnrc ~/
+
+ln -s ~/.dotfiles/.zshrc ~/
+
+ln -s ~/.dotfiles/.bashrc ~/
+
+ln -s ~/.dotfiles/.zprofile ~/
+
+ln -s ~/.dotfiles/.vscode ~/
+
+ln -s ~/.dotfiles/.conda ~/
+
 mkdir -p ~/.config/systemd/user/ && cp ./.config/systemd/user/ssh-agent.service ~/.config/systemd/user/
 
 systemctl --user enable ssh-agent
