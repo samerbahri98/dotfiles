677b24680befc7f698383472b712629028f66c5b
samerbahri98 <samer.bahri@thebahrimedia.com>
2023-01-06 23:10:48 +0100

    Last Sync: 2023-01-06 23:10:48

--- .local/bin/sync_notes.sh
+++ .local/bin/sync_notes.sh
@@ -20,3 +20,21 @@ git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S")"
 
 git push -q
 
+ZK_PATH="~/.dotfiles"
+
+cd "$ZK_PATH"
+
+CHANGES_EXIST="$(git status --porcelain | wc -l)"
+
+if [ "$CHANGES_EXIST" -eq 0 ]; then
+                exit 0
+fi
+
+git pull
+
+git add .
+
+git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S")"
+
+git push -q
+
